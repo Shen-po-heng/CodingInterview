592df785d915bfeffed9102c6a05ecbe1b370574
ShenPaul <shenpaulheng@gmail.com>
2022-01-27 14:54:26 +0100

    0127_again

--- LeetCode/Day01_0127/566ReshapetheMatrix.cpp
+++ LeetCode/Day01_0127/566ReshapetheMatrix.cpp
@@ -1,4 +1,29 @@
 //566. Reshape the Matrix
+//My method
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m = mat.size(), n = mat[0].size();
+        if (m * n != r * c) {
+            return mat;
+        }
+
+        vector<int> line(m*n);
+        int num=0;
+        for(int i=0; i<m;i++)
+            for(int j=0; j<n;j++)
+                line[num++]=mat[i][j];
+        
+        //reshape
+        vector<vector<int>> ans(r, vector<int>(c, 0));
+        num=0;
+        for(int i=0; i<r;i++)
+            for(int j=0; j<c;j++)
+                ans[i][j]=line[num++];
+        return ans;
+    }
+   
+};
 //need to think again.....by alexander
 class Solution {
 public:
