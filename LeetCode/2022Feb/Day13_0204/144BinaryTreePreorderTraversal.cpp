3828ee47b9d34c36ed6a28e5131adaebd7adb8e7
ShenPaul <shenpaulheng@gmail.com>
2022-02-04 17:32:51 +0100

    0204 Tree basic traversal

---
+++ LeetCode/2022Feb/Day13_0204/144BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,30 @@
+//144. Binary Tree Preorder Traversal
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void recursive(TreeNode* root, vector<int> &arr) {
+        if(root == NULL)
+            return;
+        else{
+            arr.push_back(root->val);
+            recursive(root->left, arr);
+            recursive(root->right, arr);
+        }
+    }
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> arr;
+        recursive(root, arr);
+        return arr;
+    }
+};
