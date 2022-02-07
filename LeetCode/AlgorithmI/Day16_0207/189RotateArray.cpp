ca75d8261ec28a030067340f180e56a7883dbe90
ShenPaul <shenpaulheng@gmail.com>
2022-02-07 02:46:47 +0100

    0207

---
+++ LeetCode/AlgorithmI/Day16_0207/189RotateArray.cpp
@@ -0,0 +1,14 @@
+//189. Rotate Array
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int arr[nums.size()];
+        for (int i = 0; i < nums.size(); i++) {
+            arr[(i + k) % nums.size()] = nums[i];
+        }
+        
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] = arr[i];
+        }
+    }
+};
