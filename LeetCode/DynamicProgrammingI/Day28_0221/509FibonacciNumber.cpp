845af522b4933981fc7cbcb0236c7123c40b5fe8
ShenPaul <shenpaulheng@gmail.com>
2022-02-21 23:51:27 +0100

    0221

---
+++ LeetCode/DynamicProgrammingI/Day28_0221/509FibonacciNumber.cpp
@@ -0,0 +1,31 @@
+//509. Fibonacci Number
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+                  
+        int arr[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+    
+        return arr[n];
+    }
+    
+};
+
+//recurison
+class Solution {
+public:
+    int fib(int n) {
+        if (N <= 1) {
+            return N;
+        }
+        return fib(N - 1) + fib(N - 2);
+    }
+    
+};
